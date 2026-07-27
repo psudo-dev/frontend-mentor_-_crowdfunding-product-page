8c0f82413e4680b2077369879292d8928fd1cbd7
Renato Setoue <renatosetoue@gmail.com>
2026-07-27 01:53:15 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
 import "../css/style.css";
+
+const projectModal = document.querySelector(
+	".project-modal",
+) as HTMLDialogElement;
+const successModal = document.querySelector(
+	".success-modal",
+) as HTMLDialogElement;
+
+projectModal?.close();
+successModal?.close();
