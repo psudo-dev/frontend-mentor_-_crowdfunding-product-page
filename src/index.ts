ffdd9a410c4d04bdba0dabbd1c3192880122442f
Renato Setoue <renatosetoue@gmail.com>
2026-08-01 19:03:18 -0300

    changed to css modules and changes style.css to index.css

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import "../css/style.css";
+import "../css/index.css";
 import { initCampaignPage } from "./render-campaign";
 import { setupListeners } from "./setup-listeners";
 import { setupMenu } from "./setup-menu";
